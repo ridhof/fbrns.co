57fec1fee999fc933d88e377dd9a8f954abc23a4
Ridho Febriansa <officialridhof@gmail.com>
2023-08-07 15:42:56 +0000

    chore: add calendar from calendly

---
+++ src/app/calendar/page.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import { Box } from '@chakra-ui/react'
+import Script from 'next/script'
+
+export default function Calendar() {
+    return (
+        <Box>
+            <Box
+                className="calendly-inline-widget"
+                data-url="https://calendly.com/ridho-fbrns/hangout" 
+                style={{ minWidth: "280px", height: "700px" }}
+            >
+            </Box>
+            <Script 
+                src="https://assets.calendly.com/assets/external/widget.js"
+            />
+        </Box>
+    )
+}
