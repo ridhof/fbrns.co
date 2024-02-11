b705682e65bd1a40e886c18b44b0956711483802
Ridho <ridho@fbrns.co>
2024-02-11 17:34:05 +0700

    fix: change calendly to calcom

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -1,20 +1,36 @@
 'use client'
 
+import Cal, { getCalApi } from '@calcom/embed-react'
 import { Box } from '@chakra-ui/react'
-import Script from 'next/script'
+import { useEffect } from 'react'
 
 export default function Calendar() {
+    useEffect(() => {
+    	(async function () {
+		const cal = await getCalApi()
+		// @ts-ignore
+		cal.ns["30min"](
+			"ui", 
+			{
+				"styles": {
+					"branding": {
+						"brandColor": "#000000"
+					}
+				},
+				"hideEventTypeDetails": false,
+				"layout": "month_view"
+			}
+		);
+	})();
+    }, [])
     return (
-        <Box>
-            <Box
-                className="calendly-inline-widget"
-                data-url="https://calendly.com/ridho-fbrns/hangout" 
-                style={{ minWidth: "280px", height: "700px" }}
-            >
-            </Box>
-            <Script 
-                src="https://assets.calendly.com/assets/external/widget.js"
-            />
+        <Box marginBottom={8}>
+	    <Cal
+	    	namespace="30min"
+		calLink="ridhof/30min"
+		style={{width:"100%",height:"100%",overflow:"scroll"}}
+		config={{layout: 'month_view'}}
+	    />
         </Box>
     )
 }
