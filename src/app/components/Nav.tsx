94c9192bb047a251b3fe5d40c95255ba68dba967
Ridho Febriansa <21301894+ridhof@users.noreply.github.com>
2024-06-23 19:18:07 +0700

    feat: add firefly on navbar

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -5,66 +5,66 @@ import { ExternalLinkIcon } from '@chakra-ui/icons'
 import React from 'react'
 
 export function Nav() {
-    return <Box>
-        <Stack 
-            direction="row" 
-            padding={2} 
+  return (
+    <Box>
+      <Stack direction='row' padding={2}>
+        <Center>
+          <Image src='/coffee.svg' width={30} height={30} alt='coffee' />
+        </Center>
+        <Heading as='h1' size='md'>
+          fbrns
+        </Heading>
+      </Stack>
+      <Stack direction='row' margin={2} padding={2} gap={6}>
+        <Link
+          as={NextLink}
+          href='/finansial'
+          color='gray.600'
+          _hover={{
+            textDecoration: 'underline',
+            textDecorationColor: 'gray.800',
+            textUnderlineOffset: '6px',
+          }}
         >
-            <Center>
-                <Image
-                    src="/coffee.svg"
-                    width={30}
-                    height={30}
-                    alt="coffee"
-                />
-            </Center>
-            <Heading as='h1' size='md'>fbrns</Heading>
-        </Stack>
-        <Stack 
-            direction="row" 
-            margin={2} 
-            padding={2} 
-            gap={6} 
+          Bantuan Finansial
+        </Link>
+        <Link
+          as={NextLink}
+          href='https://chat.fbrns.co'
+          color='gray.600'
+          _hover={{
+            textDecoration: 'underline',
+            textDecorationColor: 'gray.800',
+            textUnderlineOffset: '6px',
+          }}
         >
-            <Link 
-                as={NextLink}
-                href="/finansial"
-                isExternal={true}
-                color="gray.600"
-                _hover={{
-                    textDecoration: "underline",
-                    textDecorationColor: "blue",
-                    textUnderlineOffset: "6px"
-                }}
-            >
-                Bantuan Finansial <Show above="sm"><ExternalLinkIcon mx="2px" /></Show>
-            </Link>
-            <Link
-                as={NextLink}
-                href="https://chat.fbrns.co"
-                isExternal={true}
-                color="gray.600"
-                _hover={{
-                    textDecoration: "underline",
-                    textDecorationColor: "teal",
-                    textUnderlineOffset: "6px"
-                }}
-            >
-                XMPP Server <Show above="sm"><ExternalLinkIcon mx="2px" /></Show>
-            </Link>
-            <Link
-                as={NextLink}
-                href="https://conversejs.fbrns.co"
-                isExternal={true}
-                color="gray.600"
-                _hover={{
-                    textDecoration: "underline",
-                    textDecorationColor: "red",
-                    textUnderlineOffset: "6px"
-                }}
-            >
-                XMPP Client <Show above="sm"><ExternalLinkIcon mx="2px" /></Show>
-            </Link>
-        </Stack>
+          XMPP Server
+        </Link>
+        <Link
+          as={NextLink}
+          href='https://conversejs.fbrns.co'
+          color='gray.600'
+          _hover={{
+            textDecoration: 'underline',
+            textDecorationColor: 'gray.800',
+            textUnderlineOffset: '6px',
+          }}
+        >
+          XMPP Client
+        </Link>
+        <Link
+          as={NextLink}
+          href='https://firefly.fbrns.co'
+          color='gray.600'
+          _hover={{
+            textDecoration: 'underline',
+            textDecorationColor: 'gray.800',
+            textUnderlineOffset: '6px',
+          }}
+        >
+          Firefly III
+        </Link>
+      </Stack>
     </Box>
+  )
 }
